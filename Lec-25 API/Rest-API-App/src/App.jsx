ccfbe602c57ce3e6574bc508af3c6aceab1d2ce9
Hensi <piyush0101nakarani@gmail.com>
2025-06-12 10:00:40 +0530

    Axios API

--- Lec-25 API/Rest-API-App/src/App.jsx
+++ Lec-25 API/Rest-API-App/src/App.jsx
@@ -3,11 +3,15 @@ import { useEffect, useState } from "react";
 function App() {
 
   const [posts, setPosts] = useState([]);
+  const [comments, setComments] = useState([]);
+  const [show, setShow] = useState(true);
+
+  const baseURL = "https://jsonplaceholder.typicode.com/";
+  const postsEndPoint = "posts"
+  const commentsEndPoint = "comments"
 
   const fetchPostData = () => {
-    const postsAPI = "https://jsonplaceholder.typicode.com/posts";
-
-    fetch(postsAPI).then(
+    fetch(baseURL + postsEndPoint).then(
       (response) => {
         return response.json();
       }).then((data) => {
@@ -19,36 +23,66 @@ function App() {
       });
   }
 
+  const fecthCommentsData = async () => {
 
+    const res = await fetch(baseURL + commentsEndPoint);
+
+    const data = await res.json();
+
+    console.log("Comment Data", data);
+    setComments(data);
+
+  }
 
   useEffect(() => {
     fetchPostData()
+    fecthCommentsData();
   }, [])
 
   return <>
-    <h1 style={{ textAlign: 'center' }}>API Called</h1>
+    <hr />
 
-    <button onClick={fetchPostData}>Fetch Posts Data</button>
+    <button onClick={() => setShow(true)}> Show Posts Data</button >
+    <button style={{ marginLeft: '10px' }} onClick={() => setShow(false)}> Show Comments Data</button >
 
     <hr />
 
+    {(show) ? <h1 style={{ textAlign: 'center' }}>Posts Data </h1> : <h1 style={{ textAlign: 'center' }}>Comment Data</h1>}
+
+
     {
-      posts.map((post, index) => {
+      (show) ? posts.map((post, index) => {
         return <div key={index}>
           <h2>{post.id}</h2>
           <h1>{post.title}</h1>
           <p>{post.body}</p>
           <hr />
         </div>
+      }) : comments.map((comment, index) => {
+        return <div key={index}>
+          <p>ID : {comment.id}  | NAME : {comment.name}</p>
+          <h6>EMAIL : {comment.email}</h6>
+          <p>{comment.body}</p>
+          <hr />
+        </div>
       })
     }
 
-    
+
+
+
   </>
 }
 
 /*
  JavaScript => fetch() => API called
+
+
+ https://jsonplaceholder.typicode.com/posts
+
+ Base URL  |  End Point
+
+ https://jsonplaceholder.typicode.com/comments
 */
 
 export default App;
