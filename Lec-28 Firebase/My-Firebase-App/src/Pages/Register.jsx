7e12b0200505e6affbc6479004d59fe68c0ad7a9
Darshan <desaidarshan616@gmail.com>
2025-07-01 12:10:18 +0530

    Firebase

---
+++ Lec-28 Firebase/My-Firebase-App/src/Pages/Register.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Register() {
+  return (
+    <div style={{ textAlign: "center" }}>
+      <h1>Register Page</h1>
+    </div>
+  );
+}
---
+++ Lec-28 Firebase/My-Firebase-App/src/firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCfA3yCjJwPLaGH37Eu1B75uU9KnWlUahI",
+  authDomain: "my-react-app-8ab9c.firebaseapp.com",
+  projectId: "my-react-app-8ab9c",
+  storageBucket: "my-react-app-8ab9c.firebasestorage.app",
+  messagingSenderId: "722782994604",
+  appId: "1:722782994604:web:dec4442ecb35bf5ead5f09",
+};
+
+export const app = initializeApp(firebaseConfig);
